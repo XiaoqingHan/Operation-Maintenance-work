4a378eaf472dad91c8849e5452de115618c7df3a
Chris Han <55549961+XiaoqingHan@users.noreply.github.com>
2026-06-26 22:50:20 +0200

    Modify backup script to use generic paths
    Updated paths in backup_data1.sh to use a generic '/path' instead of specific directories.

--- backup_data1.sh
+++ backup_data1.sh
@@ -2,9 +2,9 @@ now_date=`date "+%Y_%m_%d"`
 now_time=`date +%s`
 
 # zf3
-ls -w 1 /zfssz3/ST_BIGDATA/autoanalysis_backup_2/P20Z10200N0157/*/*_backup/logs/stdout > stdout_all.list
+ls -w 1 /path/*_backup/logs/stdout > stdout_all.list
 # jd2
-ls -w 1 /jdfssz2/ST_BIGDATA/Stereomics/autoanalysis_backup/P20Z10200N0157/null/*_backup/logs/stdout > stdout_all.list
+ls -w 1 /path/*_backup/logs/stdout > stdout_all.list
 echo ${now_date}_removed.list
 
 # get remove list:
